c502f0160154351c3405b48c26d1befe3ec95de6
programmercave0 <mailabhi593@gmail.com>
2022-09-10 20:47:16 +0530

    python deleted

--- python/oop/SOLID/bird/Eagle.py
+++
@@ -1,9 +0,0 @@
-from Bird import *
-from BirdType import *
-
-class Eagle(Bird):
-	def __init__(self, weight, colour, size, beakType, birdType):
-		super().__init__(weight, colour, size, beakType, birdType)
-
-	def fly(self):
-		print("Eagle is flying")
--- python/oop/inheritance/StudentStatus.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class StudentStatus(Enum):
-	ACTIVE = 1
-	PAUSED = 1
-	COMPLETED = 1
