d00c05c7e13ee17e2ee009aef89fa1ed3ff52565
Tanmay <tanmaykacker40@gmail.com>
2022-11-08 14:28:24 +0000

    Adds wait.

--- networks/code/server.py
+++ networks/code/server.py
@@ -1,24 +1,28 @@
 import socket
 import argparse
 from threading import Thread
+import time
+
 
 def handleConnection(conn) -> None:
     print(f"Connection from {conn.getsockname()} to {conn.getpeername()}")
-        
+
     data = conn.recv(1024)
     print(f"Received data: {data}")
-        
+
+    time.sleep(5)
     conn.sendall(data.upper())
     conn.close()
 
-def main(host: str, port:int) -> None:
+
+def main(host: str, port: int) -> None:
 
     # Create a socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Bind the socket to the port
     try:
         sock.bind((host, port))
-    except socket.error as e :
+    except socket.error as e:
         print(f"Error in binding host and port: {e}")
 
     # Listen for incoming connections
@@ -34,17 +38,17 @@ def main(host: str, port:int) -> None:
         thread.start()
 
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    
     # Declare arguments
-    parser = argparse.ArgumentParser(description='Socket server')
-    parser.add_argument('-p', '--port', help='Port to bind to', required=True, type=int)
-    parser.add_argument('-H', '--host', help='Host to bind to', default='127.0.0.1', type=str)
+    parser = argparse.ArgumentParser(description="Socket server")
+    parser.add_argument("-p", "--port", help="Port to bind to", required=True, type=int)
+    parser.add_argument(
+        "-H", "--host", help="Host to bind to", default="127.0.0.1", type=str
+    )
 
     # Parse arguments
     args = parser.parse_args()
     host, port = args.host, args.port
 
     main(host, port)
-
